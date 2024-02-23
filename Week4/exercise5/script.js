8e7e290481df4189b425fb33bc1933c8bea1cbc9
Taner Dincer <dincer.taner123@gmail.com>
2024-02-23 17:09:39 +0000

    Add exercise5

---
+++ Week4/exercise5/script.js
@@ -0,0 +1,25 @@
+function print5() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+}
+
+function changeHeader(newHeader) {
+    var element = document.getElementById("page-heading");
+    element.firstChild.nodeValue = newHeader;
+}
+
+function changeParagraph(newHeader) {
+    var element = document.getElementById("page-p");
+    element.firstChild.nodeValue = newHeader;
+}
+
+
+document.addEventListener("click", () => {
+    changeHeader("New heading");
+    changeParagraph("New p");
+});
+// changeHeader("New heading");
+

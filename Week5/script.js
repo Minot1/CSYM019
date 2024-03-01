8537a2a5e626faeaa32dfcb33ee74781a45fa242
Taner Dincer <dincer.taner123@gmail.com>
2024-03-01 17:18:33 +0000

    Add ex5

--- Week5/script.js
+++ Week5/script.js
@@ -1,11 +1,18 @@
 function whenLoaded() {
     var element = document.getElementById("circle");
-    element.style.opacity = 0.5;
+    element.style.opacity = 1;
     element.addEventListener("click", () => {
         element.style.opacity = parseFloat(element.style.opacity) + 0.1;
     });
 }
 
+function myInterval() {
+    var element = document.getElementById("circle");
+    element.style.opacity = parseFloat(element.style.opacity) - 0.01;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     whenLoaded();
 });
+
+setInterval(myInterval, 100)

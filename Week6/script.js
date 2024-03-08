39085e62d46b208b3aef0cdbb165ce0d66f73a61
Taner Dincer <dincer.taner123@gmail.com>
2024-03-08 15:37:52 +0000

    Add week6 ex9

--- Week6/script.js
+++ Week6/script.js
@@ -1,5 +1,24 @@
 function keyDown(event) {
-    console.log(event.keyCode);
+    if (event.keyCode == 37) {
+        var elm = document.getElementById("circle");
+        var posLeft = elm.offsetLeft;
+        elm.style.left = posLeft - 10 + 'px';
+    }
+    if (event.keyCode == 38) {
+        var elm = document.getElementById("circle");
+        var posLeft = elm.offsetTop;
+        elm.style.top = posLeft - 10 + 'px';
+    }
+    if (event.keyCode == 39) {
+        var elm = document.getElementById("circle");
+        var posLeft = elm.offsetLeft;
+        elm.style.left = posLeft + 10 + 'px';
+    }
+    if (event.keyCode == 40) {
+        var elm = document.getElementById("circle");
+        var posLeft = elm.offsetTop;
+        elm.style.top = posLeft + 10 + 'px';
+    }
 }
 
 function whenLoaded() {

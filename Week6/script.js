f0178e560d520715d063d719f6be3ef83961070e
Taner Dincer <dincer.taner123@gmail.com>
2024-03-08 16:46:31 +0000

    Add week6 ex10

--- Week6/script.js
+++ Week6/script.js
@@ -1,13 +1,19 @@
+var timerLeft = 0, timerUp, timerRight, timerDown;
+
 function keyDown(event) {
     if (event.keyCode == 37) {
-        var elm = document.getElementById("circle");
-        var posLeft = elm.offsetLeft;
-        elm.style.left = posLeft - 10 + 'px';
+        timerLeft = setInterval(() => {
+            var elm = document.getElementById("circle");
+            var posLeft = elm.offsetLeft;
+            elm.style.left = posLeft - 10 + 'px';
+        }, 100);
     }
     if (event.keyCode == 38) {
-        var elm = document.getElementById("circle");
-        var posLeft = elm.offsetTop;
-        elm.style.top = posLeft - 10 + 'px';
+        timerUp = setInterval(() => {
+            var elm = document.getElementById("circle");
+            var posLeft = elm.offsetTop;
+            elm.style.top = posLeft - 10 + 'px';
+        }, 100);
     }
     if (event.keyCode == 39) {
         var elm = document.getElementById("circle");
@@ -21,8 +27,24 @@ function keyDown(event) {
     }
 }
 
+function keyUp(event) {
+    if (event.keyCode == 37) {
+        clearInterval(timerLeft);
+    }
+    if (event.keyCode == 38) {
+        clearInterval(timerUp);
+    }
+    if (event.keyCode == 39) {
+        
+    }
+    if (event.keyCode == 40) {
+        
+    }
+}
+
 function whenLoaded() {
     document.addEventListener("keydown", keyDown);
+    document.addEventListener("keyup", keyUp);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
